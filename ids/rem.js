1a85e01cfecc926f5ec3d7b7fdef1343ae4f462a
silence <silence@silencedeMBP.lan>
2016-04-10 23:04:25 +0800

    11111

---
+++ ids/rem.js
@@ -0,0 +1,19 @@
+(function(d, c) {
+	var e = d.documentElement,
+		b = "orientationchange" in window ? "orientationchange" : "resize",
+		a = function() {
+			var f = e.clientWidth;
+			if(f>=640){
+				f=640;
+			}
+			if (!f) {
+				return
+			}
+			e.style.fontSize = 100 * (f / 320) + "px"
+		};
+	if (!d.addEventListener) {
+		return
+	}
+	c.addEventListener(b, a, false);
+	d.addEventListener("DOMContentLoaded", a, false)
+})(document, window);
