240918db4abf8fc19395e3aeaaf4a4d56d01cf95
silencem <357003671@qq.com>
2017-01-10 04:15:55 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log('test')
+$(document).ready(function(){
+  $('.gif-img').each(function(){
+    var $el = $(this)
+    var src = $el.attr('src');
+    $el.hover(
+      function(){
+        $(this).attr("src", src.replace('.png', '.gif'));
+      },
+      function(){
+        $(this).attr("src", src);
+      });
+  });
+});
